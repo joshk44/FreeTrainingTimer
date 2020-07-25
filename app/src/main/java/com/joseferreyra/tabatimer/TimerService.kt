7bc356d4141e8b3cacf558b0c14adda26c445f80
Jose Ferreyra <joshk44@gmail.com>
2020-07-25 14:09:09 -0300

    Add sounds.

--- app/src/main/java/com/joseferreyra/tabatimer/TimerService.kt
+++ app/src/main/java/com/joseferreyra/tabatimer/TimerService.kt
@@ -2,6 +2,7 @@ package com.joseferreyra.tabatimer
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Binder
 import android.os.CountDownTimer
 import android.os.IBinder
@@ -24,6 +25,18 @@ class TimerService : Service() {
     private var timer: CountDownTimer? = null
 
     private var counterStatus: COUNTERSTATUS? = null
+
+    private var mediaPlayerBump: MediaPlayer? = null
+    private var mediaPlayerBim: MediaPlayer? = null
+    private var mediaPlayerFinish: MediaPlayer? = null
+
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayerBim = MediaPlayer.create(this, R.raw.bim)
+        mediaPlayerBump = MediaPlayer.create(this, R.raw.bump)
+        mediaPlayerFinish= MediaPlayer.create(this, R.raw.fin)
+    }
 
     override fun onBind(intent: Intent): IBinder? {
         return myBinder
@@ -50,6 +63,7 @@ class TimerService : Service() {
     private fun setupTimer(seconds: Int, type: COUNTERTYPE) {
         if (timer == null)
             timer = object : CountDownTimer(seconds.times(1000).toLong(), 100) {
+
                 override fun onFinish() {
                     onTimerFinish(type)
                 }
@@ -58,13 +72,24 @@ class TimerService : Service() {
                     millisUntilFinished.div(1000).let {
                         if (cacheSeconds != it) {
                             cacheSeconds = it
+                            Log.d("Timer", "tick $it $type")
+                            when (cacheSeconds) {
+                                1L, 2L -> {
+                                    mediaPlayerBump?.seekTo(0)
+                                    mediaPlayerBump?.start()
+                                }
+                                0L -> {
+                                    mediaPlayerBim?.seekTo(0)
+                                    mediaPlayerBim?.start()
+                                }
+                            }
                             listener?.onTick(it, type)
                             Log.d("Timer", "tick $it $type")
                         }
                     }
                 }
             }.start()
-            counterStatus = COUNTERSTATUS.RUNNING
+        counterStatus = COUNTERSTATUS.RUNNING
     }
 
     private fun onTimerFinish(type: COUNTERTYPE) {
@@ -85,6 +110,7 @@ class TimerService : Service() {
             }
         } else {
             Log.d("Timer", "finish total")
+            mediaPlayerFinish?.start()
             counterStatus = COUNTERSTATUS.ENDED
             listener?.onFinish()
             currentStep = 0
