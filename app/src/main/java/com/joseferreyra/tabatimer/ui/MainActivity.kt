1282f7914f87423179a31679e30e2d859e765ffa
Jose Ferreyra <joshk44@gmail.com>
2020-08-08 01:14:01 -0300

    Add view bindings

--- app/src/main/java/com/joseferreyra/tabatimer/ui/MainActivity.kt
+++ app/src/main/java/com/joseferreyra/tabatimer/ui/MainActivity.kt
@@ -7,105 +7,44 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.joseferreyra.tabatimer.R
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.onboarding.*
+import com.joseferreyra.tabatimer.databinding.ActivityMainBinding
+import com.joseferreyra.tabatimer.extensions.onSwipeListener
 
 const val SHAREPREF = "TABATIMER"
 const val ONBOARDINGSHOWN = "onboarding"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         showOnBoarding()
 
-        goButton.setOnClickListener {
-            if (!excerciseTime?.text.isNullOrEmpty() && excerciseTime?.text != getString(R.string.active) && excerciseTime?.text != "0" &&
-                    !restTime?.text.isNullOrEmpty() && restTime?.text != getString(R.string.rest) && restTime?.text != "0" &&
-                    !laps?.text.isNullOrEmpty() && laps?.text != getString(R.string.laps) && laps?.text != "0" ) {
+        binding.goButton.setOnClickListener {
+            if (!binding.excerciseTime.text.isNullOrEmpty() && binding.excerciseTime.text != getString(R.string.active) && binding.excerciseTime?.text != "0" &&
+                    !binding.restTime.text.isNullOrEmpty() && binding.restTime.text != getString(R.string.rest) && binding.restTime.text != "0" &&
+                    !binding.laps.text.isNullOrEmpty() && binding.laps.text != getString(R.string.laps) && binding.laps.text != "0" ) {
                 val intent = Intent(this@MainActivity, TimerRunningActivity::class.java)
                 intent.putExtra(DATA, intArrayOf(
-                        excerciseTime?.text.toString().toInt(),
-                        restTime?.text.toString().toInt(),
-                        laps?.text.toString().toInt()))
+                        binding.excerciseTime.text.toString().toInt(),
+                        binding.restTime.text.toString().toInt(),
+                        binding.laps.text.toString().toInt()))
                 startActivity(intent)
             } else {
                 Toast.makeText(this, getString(R.string.info), Toast.LENGTH_LONG).show()
             }
         }
 
-        excerciseTime?.setOnTouchListener(object : OnSwipeTouchListener() {
-            override fun onSwipeRight() {
-                super.onSwipeRight()
-                excerciseTime.setText(excerciseTime?.text?.toString()?.let {
-                    when (it.isNullOrBlank() || it == getString(R.string.active)) {
-                        true -> "5"
-                        else -> "${it.toInt() + 5}"
-                    }
-                })
-            }
+        binding.excerciseTime.onSwipeListener(5, getString(R.string.active))
+        binding.restTime.onSwipeListener(5, getString(R.string.rest))
+        binding.laps.onSwipeListener(1, getString(R.string.laps))
 
-            override fun onSwipeLeft() {
-                super.onSwipeLeft()
-                excerciseTime.setText(excerciseTime?.text?.toString()?.let {
-                    when (it.isNullOrBlank() || it == getString(R.string.active)) {
-                        true -> "0"
-                        else -> "${if (it.toInt() > 4) it.toInt() - 5 else 0}"
-                    }
-                })
-            }
-
-        })
-
-        restTime?.setOnTouchListener(object : OnSwipeTouchListener() {
-            override fun onSwipeRight() {
-                super.onSwipeRight()
-                restTime.setText(restTime?.text?.toString()?.let {
-                    when (it.isNullOrBlank() || it == getString(R.string.rest)) {
-                        true -> "5"
-                        else -> "${it.toInt() + 5}"
-                    }
-                })
-            }
-
-            override fun onSwipeLeft() {
-                super.onSwipeLeft()
-                restTime.setText(restTime?.text?.toString()?.let {
-                    when (it.isNullOrBlank() || it == getString(R.string.rest)) {
-                        true -> "5"
-                        else -> "${if (it.toInt() > 4) it.toInt() - 5 else 0}"
-                    }
-                })
-            }
-
-        })
-
-        laps?.setOnTouchListener(object : OnSwipeTouchListener() {
-            override fun onSwipeRight() {
-                super.onSwipeRight()
-                laps.setText(laps?.text?.toString()?.let {
-                    when (it.isNullOrBlank() || it == getString(R.string.laps)) {
-                        true -> "1"
-                        else -> "${it.toInt() + 1}"
-                    }
-                })
-            }
-
-            override fun onSwipeLeft() {
-                super.onSwipeLeft()
-                laps.setText(laps?.text?.toString()?.let {
-                    when (it.isNullOrBlank() || it == getString(R.string.laps)) {
-                        true -> "0"
-                        else -> "${if (it.toInt() > 0) it.toInt() - 1 else 0}"
-                    }
-                })
-            }
-
-        })
-
-        historicalFloating.setOnClickListener {
+        binding.historicalFloating.setOnClickListener {
             startActivity(Intent (this, HistoricalActivity::class.java))
         }
     }
@@ -113,9 +52,9 @@ class MainActivity : AppCompatActivity() {
     private fun showOnBoarding() {
         val sharedPreferences = getSharedPreferences(SHAREPREF, Context.MODE_PRIVATE)
         if (!sharedPreferences.getBoolean(ONBOARDINGSHOWN, false)) {
-            onboardinng.visibility = View.VISIBLE
-            gotit.setOnClickListener {
-                onboardinng.visibility = View.GONE
+            binding.onboardinng.onboarding.visibility = View.VISIBLE
+            binding.onboardinng.gotit.setOnClickListener {
+                binding.onboardinng.onboarding.visibility = View.GONE
                 sharedPreferences.edit().putBoolean(ONBOARDINGSHOWN, true).apply()
             }
         }
