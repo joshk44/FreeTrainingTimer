3be56676a0d2634b1e2344113dcef51db17fad3f
Jose Ferreyra <joshk44@gmail.com>
2020-08-08 01:14:20 -0300

    Add extensions

--- app/src/main/java/com/joseferreyra/tabatimer/extensions/extensions.kt
+++ app/src/main/java/com/joseferreyra/tabatimer/extensions/extensions.kt
@@ -1,11 +1,17 @@
 package com.joseferreyra.tabatimer.extensions
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.constraintlayout.widget.Placeholder
 import androidx.recyclerview.widget.RecyclerView
+import com.joseferreyra.tabatimer.R
+import com.joseferreyra.tabatimer.ui.OnSwipeTouchListener
 
 fun Context.toast (message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
@@ -17,3 +23,30 @@ fun RecyclerView.ViewHolder.toast (message: String) {
 
 fun ViewGroup.inflate (@LayoutRes id: Int) = LayoutInflater.from(context).inflate(id, this, false)
 
+
+@SuppressLint("ClickableViewAccessibility")
+fun TextView.onSwipeListener (increment: Int, placeholder: String) {
+
+    this.setOnTouchListener(object : OnSwipeTouchListener() {
+        override fun onSwipeRight() {
+            super.onSwipeRight()
+            this@onSwipeListener.text = (this@onSwipeListener.text?.toString()?.let {
+                when (it.isNullOrBlank() || it == placeholder) {
+                    true -> increment.toString()
+                    else -> "${it.toInt() + increment}"
+                }
+            })
+        }
+
+        override fun onSwipeLeft() {
+            super.onSwipeLeft()
+            this@onSwipeListener.setText(this@onSwipeListener.text?.toString()?.let {
+                when (it.isNullOrBlank() || it == placeholder) {
+                    true -> "0"
+                    else -> "${if (it.toInt() > 0) it.toInt() - increment else 0}"
+                }
+            })
+        }
+
+    })
+}
