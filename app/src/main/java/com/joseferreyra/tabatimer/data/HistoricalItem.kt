c1f9f4016620dc4ab7c84e86a5d1854f51ac7759
Jose Ferreyra <joshk44@gmail.com>
2020-08-10 01:34:52 -0300

    Add extension.

--- app/src/main/java/com/joseferreyra/tabatimer/data/HistoricalItem.kt
+++ app/src/main/java/com/joseferreyra/tabatimer/data/HistoricalItem.kt
@@ -5,3 +5,10 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class HistoricalItem (val active: Int, val rest: Int, val laps: Int) : Parcelable
+
+fun HistoricalItem.toStoredValue () = "$active,$rest,$laps"
+
+fun String.toHistoricalItem () =
+    this.split(",")?.map { it.toInt() }.let {
+        HistoricalItem(it[0], it[1], it[2])
+    }
