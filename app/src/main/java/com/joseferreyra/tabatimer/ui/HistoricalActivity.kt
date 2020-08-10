e98f0504aa8ac9e5b14b8b03823f3cd82145c17b
Jose Ferreyra <joshk44@gmail.com>
2020-08-10 01:34:02 -0300

    Add redirect from history

--- app/src/main/java/com/joseferreyra/tabatimer/ui/HistoricalActivity.kt
+++ app/src/main/java/com/joseferreyra/tabatimer/ui/HistoricalActivity.kt
@@ -1,10 +1,13 @@
 package com.joseferreyra.tabatimer.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.joseferreyra.tabatimer.R
 import com.joseferreyra.tabatimer.data.HistoricalDataSource
+import com.joseferreyra.tabatimer.data.HistoricalItem
 import kotlinx.android.synthetic.main.activity_historical.*
 
 class HistoricalActivity : AppCompatActivity() {
@@ -12,7 +15,16 @@ class HistoricalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historical)
-        rvHistorical.adapter = HistoricalAdapter(HistoricalDataSource().historicalItems)
+        rvHistorical.adapter = HistoricalAdapter(HistoricalDataSource(sharedPreferences = getSharedPreferences("HISTORICAL", Context.MODE_PRIVATE)).getList()) {redirectToTimer(it) }
         rvHistorical.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun redirectToTimer (historicalItem: HistoricalItem) {
+        startActivity (Intent(this, TimerRunningActivity::class.java).apply {
+            this.putExtra(DATA, intArrayOf(
+                    historicalItem.active,
+                    historicalItem.rest,
+                    historicalItem.laps))
+        } )
     }
 }
