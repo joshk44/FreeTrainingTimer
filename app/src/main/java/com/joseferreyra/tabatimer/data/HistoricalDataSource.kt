febcf024cd496fc05f8aad2c26fbb02767340063
Jose Ferreyra <joshk44@gmail.com>
2020-08-10 01:34:33 -0300

    Add historical data source.

--- app/src/main/java/com/joseferreyra/tabatimer/data/HistoricalDataSource.kt
+++ app/src/main/java/com/joseferreyra/tabatimer/data/HistoricalDataSource.kt
@@ -1,11 +1,26 @@
 package com.joseferreyra.tabatimer.data
 
-class HistoricalDataSource {
+import android.content.SharedPreferences
 
-    var historicalItems: List<HistoricalItem> = listOf( HistoricalItem(10,10,10),
-            HistoricalItem(20,10,10),
-            HistoricalItem(30,10,10),
-            HistoricalItem(40,10,10),
-            HistoricalItem(50,10,10)
-                )
+class HistoricalDataSource (private val sharedPreferences: SharedPreferences) {
+
+    fun store(historicalItem: HistoricalItem) {
+        val stored = sharedPreferences.getStringSet("HISTORIC", emptySet())
+
+
+        val newStored = stored?.toMutableSet()?.run {
+            if (this.contains(historicalItem.toStoredValue())) this.remove(historicalItem.toStoredValue())
+            this.add(historicalItem.toStoredValue())
+            if (this.size > 10)
+                this.remove(this.first())
+            this
+        }
+
+        sharedPreferences.edit().putStringSet("HISTORIC", newStored).apply()
+    }
+
+    fun getList() =
+            with(sharedPreferences.getStringSet("HISTORIC", emptySet())) {
+                this?.map { it.toHistoricalItem() }
+            } ?: emptyList()
 }
