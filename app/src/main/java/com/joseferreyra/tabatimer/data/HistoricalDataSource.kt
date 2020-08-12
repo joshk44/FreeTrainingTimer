5587098a6b09bef7171cbf735b42ca23fb6f0991
Jose Ferreyra <joshk44@gmail.com>
2020-08-11 21:13:34 -0300

    Fix Storage

--- app/src/main/java/com/joseferreyra/tabatimer/data/HistoricalDataSource.kt
+++ app/src/main/java/com/joseferreyra/tabatimer/data/HistoricalDataSource.kt
@@ -7,15 +7,13 @@ class HistoricalDataSource (private val sharedPreferences: SharedPreferences) {
     fun store(historicalItem: HistoricalItem) {
         val stored = sharedPreferences.getStringSet("HISTORIC", emptySet())
 
-
         val newStored = stored?.toMutableSet()?.run {
             if (this.contains(historicalItem.toStoredValue())) this.remove(historicalItem.toStoredValue())
-            this.add(historicalItem.toStoredValue())
             if (this.size > 10)
                 this.remove(this.first())
+            this.add(historicalItem.toStoredValue())
             this
         }
-
         sharedPreferences.edit().putStringSet("HISTORIC", newStored).apply()
     }
 
